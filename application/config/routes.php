ca8dae7eb563e6fcfa784351ed12337c7284f7ed
Jeff Verkoeyen <jverkoey@gmail.com>
2010-10-24 15:22:01 -0400

    Add gallery controller.

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,5 +5,5 @@ $config['article/([a-zA-Z0-9\-]+)'] = 'article/show/$1';
 $config['showcase/([a-zA-Z0-9\-]+)'] = 'home/showcase/$1';
 $config['module/([a-zA-Z0-9\-]+)'] = 'module/show/$1';
 $config['roadmap/([a-zA-Z0-9\.\-]+)'] = 'roadmap/show/$1';
-$config['(overview)'] = 'home/redirect/$1';
+$config['(overview|contribute)'] = 'home/redirect/$1';
 $config['(setup|core|ui|timeline)/(.+)'] = 'home/redirect/$1_$2';
