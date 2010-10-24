2ee012f138db64da14146ed4632c688d72e38d82
Jeff Verkoeyen <jverkoey@gmail.com>
2010-10-24 19:11:46 -0400

    Remove sublayouts autogenerated dir.

--- application/views/sublayouts/apibar.php
+++
@@ -1,5 +0,0 @@
-<div class="horizontal-bar">
-<p>
-  <a href="http://api.Three20.info/">View the Three20 API documentation</a>
-</p>
-</div> <!-- .horizontal-bar -->
--- application/views/sublayouts/contribute.php
+++
@@ -1,7 +0,0 @@
-<div class="box">
-  <div class="title">Contribute</div>
-  <ul>
-    <li><a href="/article/2010-10-15-Editing-A-GitHub-Article">How to Edit a GitHub Article</a></li>
-    <li><a href="/article/2010-10-24-Branches-And-Cuts">Branches and Cuts</a></li>
-  </ul>  
-</div> <!-- .box -->
