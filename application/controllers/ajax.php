452ccd55ae6fe38151a23022cdacb5c96b3413a3
jverkoey <jverkoey@gmail.com>
2010-12-14 09:05:26 -0500

    Refactor the ajax endpoint a bit.

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -2,6 +2,8 @@
 
 define('GITHUB_DATA_DB_REFRESH', 5);
 define('GITHUB_DATA_FETCH_REFRESH', 5*60);
+define('GITHUB_USERNAME_REGEX', '/^[a-z0-9_]+$/i');
+define('GITHUB_REPONAME_REGEX', '/^[a-z0-9_]+$/i');
 
 /**
  * @package    three20
@@ -13,7 +15,7 @@ class Ajax_Controller extends Three20_Controller {
 
   const ALLOW_PRODUCTION = TRUE;
 
-  public function fetch_data_from_url($url, $stale_age) {
+  protected function fetch_data_from_url($url, $stale_age) {
     $cachePath = APPPATH.'cache/';
     $cacheKey = sha1($url);
     $cacheFilename = $cachePath.$cacheKey;
@@ -36,13 +38,49 @@ class Ajax_Controller extends Three20_Controller {
     return $data;
   }
 
+  /**
+   * Fetch the repo information from the database, if it exists.
+   *
+   * @return null if no repo with this info exists in the db
+   */
+  protected function get_db_repo_info($db, $username, $reponame) {
+    $result = $db->
+      from('repos')->
+      select(array('repoid', 'description', 'homepage', 'last_updated'))->
+      where('username', $username)->
+      where('reponame', $reponame)->
+      limit(1)->
+      get();
+
+    $repo = null;
+
+    if (count($result)) {
+      // TODO: Is there a simpler way to do this?
+      $repodbobj = null;
+      foreach ($result as $row) {
+        $repodbobj = $row;
+        break;
+      }
+
+      // TODO: Is there a simpler, equivalently specific way to do this?
+      $repo = array(
+        'repoid'        => $repodbobj->repoid,
+        'description'   => $repodbobj->description,
+        'homepage'      => $repodbobj->homepage,
+        'last_updated'  => $repodbobj->last_updated
+      );
+    }
+
+    return $repo;
+  }
+
   // Welcome to clowntown. This method is insanely long. It basically does some caching of
   // github info in a db and returns the results in JSON form.
   public function github($username, $reponame) {
     header('Content-type: application/json');
 
-    if (!preg_match('/^[a-z0-9_]+$/i', $username) ||
-        !preg_match('/^[a-z0-9_]+$/i', $reponame)) {
+    if (!preg_match(GITHUB_USERNAME_REGEX, $username) ||
+        !preg_match(GITHUB_REPONAME_REGEX, $reponame)) {
       echo json_encode(array('error' => "Invalid username or reponame"));
       exit;
     }
@@ -54,42 +92,21 @@ class Ajax_Controller extends Three20_Controller {
       'username' => $username,
       'reponame' => $reponame
     );
-    $repoid = null;
+
     // Assume the data is stale/nonexistent.
     $data_is_stale = true;
 
-    // Check if the repo exists.
-    $result = $db->
-      from('repos')->
-      select(array('repoid', 'description', 'homepage', 'last_updated'))->
-      where('username', $username)->
-      where('reponame', $reponame)->
-      limit(1)->
-      get();
+    $repoinfo = $this->get_db_repo_info($db, $username, $reponame);
 
-    if (count($result)) {
-      // The repo exists!
-      $repodbobj = null;
-      foreach ($result as $row) {
-        $repodbobj = $row;
-        break;
-      }
-      
-      // Grab the available information.
-      $repoid = $repodbobj->repoid;
-      $repo['repoid'] = $repoid;
-      $repo['description'] = $repodbobj->description;
-      $repo['homepage'] = $repodbobj->homepage;
-      $repo['last_updated'] = $repodbobj->last_updated;
-      
-      // Does the repo need to be updated?
-      $last_updated = strtotime($repo['last_updated']);
+    if ($repoinfo) {
+      $repo = array_merge($repo, $repoinfo);
+
+      $last_updated = strtotime($repoinfo['last_updated']);
       if (time() - $last_updated <= GITHUB_DATA_DB_REFRESH) {
-        // Yes, it does.
         $data_is_stale = false;
       }
     }
-    
+
     if ($data_is_stale) {
       // Let's check the local cache to see if we have any non-stale github data then.
       $url = 'http://github.com/api/v2/json/repos/show/'.$username.'/'.$reponame.'/tags';
@@ -117,7 +134,7 @@ class Ajax_Controller extends Three20_Controller {
       $repo['homepage'] = $repoobj['repository']['homepage'];
       
       // Creating a new repo?
-      if (!$repoid) {
+      if (!$repo['repoid']) {
         // Yes, fire it off to the db.
         $last_updated = date("Y-m-d H:i:s");
         $repodbobj = array(
@@ -129,13 +146,12 @@ class Ajax_Controller extends Three20_Controller {
         );
         $repodbinfo = $db->insert('repos', $repodbobj);
         
-        $repoid = $repodbinfo->insert_id();
-        $repo['repoid'] = $repoid;
+        $repo['repoid'] = $repodbinfo->insert_id();
         $repo['last_updated'] = $last_updated;
 
         foreach ($tagobj['tags'] as $tagname => $tagsha1) {
           $tagdbobj = array(
-            'repoid' => $repoid,
+            'repoid' => $repo['repoid'],
             'tagname' => $tagname,
             'tagsha1' => $tagsha1,
           );
@@ -150,14 +166,14 @@ class Ajax_Controller extends Three20_Controller {
           set('description', $repo['description'])->
           set('homepage', $repo['homepage'])->
           set('last_updated', $repo['last_updated'])->
-          where('repoid', $repoid)->
+          where('repoid', $repo['repoid'])->
           update();
 
         // Get the list of known tags.
         $result = $db->
           from('tags')->
           select(array('tagname', 'tagsha1'))->
-          where('repoid', $repoid)->
+          where('repoid', $repo['repoid'])->
           get();
 
         // Simplify the result.
@@ -173,12 +189,12 @@ class Ajax_Controller extends Three20_Controller {
         foreach ($dbtags as $tagname => $tagsha1) {
           if (!isset($repo['tags'][$tagname])) {
             // If the tag doesn't exist in the new tag list, delete it from the db.
-            $result = $db->delete('tags', array('repoid' => $repoid, 'tagname' => $tagname));
+            $result = $db->delete('tags', array('repoid' => $repo['repoid'], 'tagname' => $tagname));
             
           } else if ($repo['tags'][$tagname] != $tagsha1) {
             // If the tag's sha1 has changed, update it in the db.
             $result = $db->update('tags', array('tagsha1' => $tagsha1),
-                                  array('repoid' => $repoid, 'tagname' => $tagname));
+                                  array('repoid' => $repo['repoid'], 'tagname' => $tagname));
           }
         }
         
@@ -186,7 +202,7 @@ class Ajax_Controller extends Three20_Controller {
         foreach ($repo['tags'] as $tagname => $tagsha1) {
           if (!isset($dbtags[$tagname])) {
             $tagdbobj = array(
-              'repoid' => $repoid,
+              'repoid' => $repo['repoid'],
               'tagname' => $tagname,
               'tagsha1' => $tagsha1,
             );
@@ -202,7 +218,7 @@ class Ajax_Controller extends Three20_Controller {
       $result = $db->
         from('tags')->
         select(array('tagname', 'tagsha1'))->
-        where('repoid', $repoid)->
+        where('repoid', $repo['repoid'])->
         get();
       
       $repo['tags'] = array();
--- application/views/kohana_error_page.php
+++ application/views/kohana_error_page.php
@@ -1,59 +1,27 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml"
-      xmlns:og="http://opengraphprotocol.org/schema/"
-      xmlns:fb="http://www.facebook.com/2008/fbml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <meta property="fb:app_id" content="114720825217072"/>
-  <meta property="fb:admins" content="122605446"/>
-  <meta property="og:title" content="three20.info"/>
-  <meta property="og:type" content="website"/>
-  <meta property="og:url" content="http://three20.info/"/>
-  <meta property="og:image" content="http://github.com/facebook/three20/raw/master/samples/Resources/Icon.png"/>
-  <meta name="keywords" content="three20 iPhone api open source library uikit tt" />
-  <meta name="description" content="API documentation, articles, and tutorials related to the three20 iPhone library." />
-  <title>Oh no! | Three20</title>
-<?
-  echo three20html::stylesheet(array(
-    'css/reset',
-    'css/global.css',
-  ), null, FALSE);
-?>
+<style type="text/css">
+<?php include Kohana::find_file('views', 'kohana_errors', FALSE, 'css') ?>
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title><?php echo $error ?></title>
+<base href="http://php.net/" />
 </head>
 <body>
-
-<div id="page-wrapper">
-
-We couldn't find the resource you're looking for. <a href="/">Click your heels three times...</a>
-
-</div> <!-- #page-wrapper -->
-
-<?php if (IN_PRODUCTION): ?>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-19295672-1']);
-  _gaq.push(['_setDomainName', '.three20.info']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-
-<script type="text/javascript">
-var disqus_shortname = 'three20info2';
-(function () {
-  var s = document.createElement('script'); s.async = true;
-  s.src = 'http://disqus.com/forums/three20info2/count.js';
-  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-}());
-</script>
-
+<div id="framework_error" style="width:42em;margin:20px auto;">
+<h3><?php echo html::specialchars($error) ?></h3>
+<p><?php echo html::specialchars($description) ?></p>
+<?php if ( ! empty($line) AND ! empty($file)): ?>
+<p><?php echo Kohana::lang('core.error_file_line', $file, $line) ?></p>
 <?php endif ?>
-
+<p><code class="block"><?php echo $message ?></code></p>
+<?php if ( ! empty($trace)): ?>
+<h3><?php echo Kohana::lang('core.stack_trace') ?></h3>
+<?php echo $trace ?>
+<?php endif ?>
+<p class="stats"><?php echo Kohana::lang('core.stats_footer') ?></p>
+</div>
 </body>
 </html>
