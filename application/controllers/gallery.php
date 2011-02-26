ac2507f5c94ccaa469bd52bdeae1d84b13cb3143
jverkoey <jverkoey@gmail.com>
2011-02-26 14:54:40 -0500

    Implement gallery submit form.

--- application/controllers/gallery.php
+++ application/controllers/gallery.php
@@ -19,4 +19,31 @@ class Gallery_Controller extends Three20_Controller {
     $this->render_article_template($content);
   }
 
+  public function submit() {
+    if (!IN_PRODUCTION) {
+      $profiler = new Profiler;
+    }
+
+    $content = new View('pages/gallerysubmit');
+
+    $this->render_article_template($content);
+  }
+
+  public function getcode() {
+    if (!IN_PRODUCTION) {
+      $profiler = new Profiler;
+    }
+
+    $content = new View('pages/gallerycode');
+
+    $content->appname = trim($this->input->post('appname'));
+    $content->companyname = trim($this->input->post('companyname'));
+    $content->website = trim($this->input->post('website'));
+    $content->itunesurl = trim($this->input->post('itunesurl'));
+    $content->description = trim($this->input->post('description'));
+    $content->websiteclean = str_replace('http://', '', str_replace('www.', '', $content->appname));
+    $content->appid = str_replace(' ', '_', strtolower($content->appname));
+
+    $this->render_article_template($content);
+  }
 }
